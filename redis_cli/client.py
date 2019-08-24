a02cbae79c052ff21feceeabe00e6b4358860a7c
Stefania Ive <stefaniaive@gmail.com>
2019-08-24 02:47:00 -0300

    update redis credentials

--- redis_cli/client.py
+++ redis_cli/client.py
@@ -1,4 +1,5 @@
 import redis
+# REDIS_URL: redis://h:p37b50b38b08e9c1972c671e325b9b4b73834301c7ef309bb009937e117cacd29@ec2-34-227-251-50.compute-1.amazonaws.com:17169
 
-redis_mines_structure = redis.StrictRedis(password="p6a4d3fbcd9f87a8800078068f301365cb3d58499ed6a45d69828d9168a57ce04", host='ec2-3-222-121-40.compute-1.amazonaws.com', port=18119, db=0)
-redis_view_structure = redis.StrictRedis(password="p6a4d3fbcd9f87a8800078068f301365cb3d58499ed6a45d69828d9168a57ce04",host='ec2-3-222-121-40.compute-1.amazonaws.com', port=18119, db=1)
+redis_mines_structure = redis.StrictRedis(password="p37b50b38b08e9c1972c671e325b9b4b73834301c7ef309bb009937e117cacd29", host='ec2-34-227-251-50.compute-1.amazonaws.com', port=17169, db=0)
+redis_view_structure = redis.StrictRedis(password="p37b50b38b08e9c1972c671e325b9b4b73834301c7ef309bb009937e117cacd29",host='ec2-34-227-251-50.compute-1.amazonaws.com', port=17169, db=1)
